5e099e465763db6d6330deade28b1dd8bfc82da1
Danz <danz@example.com>
2026-06-16 23:51:53 +0800

    Remove check_scripts.php

--- check_scripts.php
+++
@@ -1,7 +0,0 @@
-<?php
-$html = file_get_contents('index.html');
-preg_match_all('/<script.*?>(.*?)<\/script>/is', $html, $matches);
-foreach($matches[1] as $i => $s) {
-    $lines = explode("\n", $s);
-    echo "Script $i Line 2: " . trim($lines[1] ?? '') . "\n";
-}
